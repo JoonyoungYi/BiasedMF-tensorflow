a7b93d00cc05c6da1a534d7dfade7bb5610bacb8
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2020-01-30 10:20:51 +0900

    update

--- calculate_similarity.py
+++ calculate_similarity.py
@@ -7,30 +7,30 @@ from app.utils.dataset import DatasetManager
 
 def main(kind):
     full = np.load('data/{}/result-q.npy'.format(kind))
+    print('>> np.std(full):', np.std(full))
+    print('>> np.mean(full):', np.mean(full))
+
     user_num = full.shape[0]
 
     user_sim = np.ones((user_num, user_num))
     for i in range(user_num):
         for j in range(i + 1, user_num):
-            i_vector = full[i]
-            j_vector = full[j]
-            s = np.dot(i_vector, j_vector) / (
-                np.linalg.norm(i_vector) * np.linalg.norm(j_vector))
-            if not math.isnan(s):
-                user_sim[j][i] = s
-                user_sim[i][j] = s
-                continue
+            i_vector = full[i] / np.mean(full[i])
+            j_vector = full[j]  / np.mean(full[j])
 
-            i_vector = full[i] * 1e20
-            j_vector = full[j] * 1e20
+            # print()
+            # print(np.mean(j_vector))
+
             s = np.dot(i_vector, j_vector) / (
                 np.linalg.norm(i_vector) * np.linalg.norm(j_vector))
-            if not math.isnan(s):
+            if not math.isnan(s) and math.isfinite(s):
                 user_sim[j][i] = s
                 user_sim[i][j] = s
             else:
-                user_sim[j][i] = -1
-                user_sim[i][j] = -1
+                print(s)
+                print(i_vector)
+                print(j_vector)
+                input('!!')
 
     # print(user_sim)
     sim_i = np.ones((user_num, user_num))
@@ -52,5 +52,6 @@ def main(kind):
 
 
 if __name__ == '__main__':
-    kind = DatasetManager.KIND_MOVIELENS_100K
+    # kind = DatasetManager.KIND_MOVIELENS_100K
+    kind = DatasetManager.KIND_MOVIELENS_1M
     main(kind)
